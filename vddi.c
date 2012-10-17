80fa00ef2a2efd7c99e7d8c92b344fa3557ec7ba
Richard H Lee <ricardohenrylee@gmail.com>
2012-10-17 20:22:16 +0100

    header string

--- vddi.c
+++ vddi.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
-char usage[] =
+
+#define HEADER_SIZE 0x200
+#define HEADER_STRING "<<< Oracle VM VirtualBox Disk Image >>>"
+
+const char usage[] =
   "Usage: to be written\n";
+
+
+void error(int line, char * file)
+{
+  printf("[%s:%i] Last set error code is %i: %s\n"
+    "Use gdb to catch this SIGTRAP\n",
+    file, line, errno, strerror(errno));
+  __asm__("int3");
+  exit(errno);
+}
+
 
 int
 main(int argc, char *argv[])
@@ -11,6 +28,10 @@ main(int argc, char *argv[])
   int optCount = 0;
   char opt;
   int infoMode = 0;
+  unsigned char headerBuffer[HEADER_SIZE];
+  FILE* vdi;
+  int vdiFd;
+  char* input, *output;
   
   while((opt = getopt(argc, argv, "i:s")) != -1)
   {
@@ -23,6 +44,7 @@ main(int argc, char *argv[])
           exit(1);
         }
         infoMode = 1;
+        input = optarg;
         break;
       case 's':
         break;
@@ -32,7 +54,6 @@ main(int argc, char *argv[])
         break;
     }
   }
-  printf("%d args left\n", argc - optind);
   
   if((infoMode && (argc != optind)) ||
     (!infoMode && ((argc - optind) != 2)))
@@ -40,5 +61,25 @@ main(int argc, char *argv[])
     printf(usage);
     exit(1);
   }
+  
+  if(!infoMode)
+  {
+    input = argv[optind];
+    output = argv[optind + 1];
+  }
+  
+  if((vdi = fopen(input, "r")) == NULL)
+    error(__LINE__, __FILE__);
+  vdiFd = fileno(vdi);
+  
+  if(read(vdiFd, headerBuffer, HEADER_SIZE) != HEADER_SIZE)
+    error(__LINE__, __FILE__);
+  
+  if(strncmp(headerBuffer, HEADER_STRING, strlen(HEADER_STRING)))
+  {
+    printf("Could not find header string\n");
+    exit(1);
+  }
+  
   return 0;
 }
